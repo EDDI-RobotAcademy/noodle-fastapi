db8d156181fda9ff4eb4df41703e6d582c423cb6
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-10-03 21:12:56 +0900

    fix: [한재혁] taskmanager 연결을 위해 app/main.py 수정 [NF-2]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
-import os
-
-import uvicorn
-
-from dotenv import load_dotenv
-from fastapi import FastAPI
+import os.path
+import sys
 from fastapi.middleware.cors import CORSMiddleware
 
+import colorama
+
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
 from generate_backlog.controller.generate_backlog_controller import generateBacklogRouter
+from user_defined_initializer.init import UserDefinedInitializer
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'template'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'template', 'include', 'socket_server'))
+
+from template.deep_learning.controller.deep_learning_controller import deepLearningRouter
+from template.dice.controller.dice_controller import diceResultRouter
+from template.system_initializer.init import SystemInitializer
+from template.task_manager.manager import TaskManager
+from template.include.socket_server.initializer.init_domain import DomainInitializer
+
+DomainInitializer.initEachDomain()
+SystemInitializer.initSystemDomain()
+UserDefinedInitializer.initUserDefinedDomain()
 
 app = FastAPI()
 
@@ -22,12 +38,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(deepLearningRouter)
+app.include_router(diceResultRouter)
+
 app.include_router(generateBacklogRouter)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
 if __name__ == "__main__":
+    colorama.init(autoreset=True)
+
+    TaskManager.createSocketServer()
     uvicorn.run(app, host=os.getenv('HOST'), port=int(os.getenv('FASTAPI_PORT')))
